6e43ef775b7d7bd4c9b6448192615b61941bc82f
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2023-11-05 15:43:56 +0100

    Added the second view

---
+++ SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitHubDeomo
+//
+//  Created by Michael Rudolf on 05.11.23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
