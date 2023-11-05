cb09ca39db7880eb3ee6b13c8cda36ce1ad56df6
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2023-11-05 15:28:01 +0100

    Changed name

---
+++ GitHubDeomo/GitHubDeomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDeomoApp.swift
+//  GitHubDeomo
+//
+//  Created by Michael Rudolf on 05.11.23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDeomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewMainView()
+        }
+    }
+}
---
+++ GitHubDeomo/NewMainView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GitHubDeomo
+//
+//  Created by Michael Rudolf on 05.11.23.
+//
+
+import SwiftUI
+
+struct NewMainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "plus")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, Git(Hub)")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NewMainView()
+}
